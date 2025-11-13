0fdb6e8516824f07463e850075e1c2a2376d9f38
MOHIT DHAMI <148179834+mohitdhami2611@users.noreply.github.com>
2025-11-13 20:59:27 +0530

    just made some changes in the authorization of login logout signup etc

--- Backend/index.js
+++
@@ -1,6 +0,0 @@
-import express from "express"
-const app = express();
-
-app.listen(5001, () => {
-    console.log("server is running on port number 5001")
-})
---
+++ Backend/src/index.js
@@ -0,0 +1,22 @@
+import express from "express"
+import router from './routes/auth.route.js'
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+
+import { connectDB } from "./lib/db.js";
+
+
+dotenv.config()
+const app = express();
+const PORT = process.env.PORT || 5001
+
+app.use(express.json())
+
+app.use("/api/auth",router)
+
+app.use(cookieParser)
+
+app.listen(PORT, () => {
+    console.log(`server is running on Port: ${PORT}`)
+    connectDB()
+})
