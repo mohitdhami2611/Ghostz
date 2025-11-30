7519dd77b24230f60ae9d4aaa916b5fcc7c3ca42
MOHIT DHAMI <148179834+mohitdhami2611@users.noreply.github.com>
2025-11-30 20:24:25 +0530

    just made a mainor change to  maintain my streak

--- Frontend/src/pages/SignUpPage.jsx
+++ Frontend/src/pages/SignUpPage.jsx
@@ -31,6 +31,8 @@ const SignUpPage = () => {
                     <div className='size-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors'>
                     <MessageSquare className='size-6 text-primary'/>
                     </div>
+                    <h1 className='text-2xl font-bold mt-2'>Create Account</h1>
+                    <p className='text-base-contnet/60'>Get Started With Your Free Account</p>
                 </div>
             </div>
         </div>
